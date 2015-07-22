35e9d0fd4dae52bccb4736243f587cc036910a20
Patrick <pcnicklaus@gmail.com>
2015-07-22 08:40:01 -0600

    working versions update as of 7.22.15

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
-console.log("sanity check!")
+// console.log("sanity check!")
+
+// function matchMaker() {
+//   var studentNumber = parseInt(prompt("How many students are learning JavaScript?"));
+//   // var studentNumber = parseInt(studentNumber);
+//   var students = {};
+//   console.log(studentNumber);
+
+// //things i'm not getting / don't understand.
+// // 1) how to create a new object for how many students the person enters.
+// // 2) how to get the information into those objects
+// // 3) can i use the persons answer to create that amount of blank objects and then prompt them for the answers?
+
+//   // for (var i = 0; i < studentNumber.length; i++) {
+//   //     deck.push({
+//   //       card: ranks[i].card,
+//   //       suit: suits[j]
+//   //     });
+
+//   while (i < studentNumber) {
+//   // for (var i = 0; i < studentNumber; i++) {
+//     student(i).name = prompt("What is the students name?")
+//     student(i).phone = prompt("What is the student's phone number?")
+//     student(i).city = prompt("What is the student's city?")
+//     i++;
+//   }
+//   console.log()
+// }
+// console.log(matchMaker());
+
+// function matchMaker2() {
+//   function getStudentInfo()
+//   var student1.name = prompt("What is the students name?")
+//   var student1.phone = prompt("What is the students phone number?")
+//   var student2.city = prompt("What is the students city?")
+
+// }
+
+// //alert total number of students and gotta create a to loop the students through this//
+// alert("Name: " + varName + "\n Phone Number: " + varNumber + "\nCity: " + varCity);
+
+// // After the output, ask the user for a city name. Display all the mentors in that city.
+// var cityCheckName = prompt("What city do you live in?")
+
+// //search through the array/object and identify all of the those cities and return them...
---
+++ test.js
@@ -0,0 +1,98 @@
+//I want to create an object for each student and then push into student array. don't need a function to start//
+  var student = [];
+  var mentor = [];
+  var studentNumber = prompt("How many students are learning JavaScript?");
+
+
+  for (var i = 0; i < studentNumber; i++) {
+
+      var namePrompt = prompt("What is the students name?");
+      var phonePrompt = prompt("what is the students phone number?");
+      var cityPrompt = prompt("What is the students city?");
+      student.push({
+        name: namePrompt
+      });
+
+      // var contactInfo = {};
+      // contactInfo.name = namePrompt;
+
+      namePrompt = namePrompt
+      console.log("name" + contactInfo)
+      phonePrompt = phonePrompt.student ;
+      cityPrompt.student = cityPrompt;
+      contactInfo.push({name: namePrompt, phone: phonePrompt, city: cityPrompt});
+      console.log("Student" + contactInfo)
+    }
+
+  // var mentorsNumber =  prompt("How many Mentors are teaching JavaScript?");
+  // for (var i = 0; i < mentorsNumber; i++) {
+  //     var contactInfoMentor = {};
+  //     var namePrompt = prompt("What is the mentors name?");
+  //     var phonePrompt = prompt("what is the mentors phone number?");
+  //     var cityPrompt = prompt("What is the mentors city?");
+  //     namePrompt.mentor = contactInfoMentor;
+  //     phonePrompt.mentor = contactInfoMentor;
+  //     cityPrompt.mentor = contactInfoMentor;
+  //     mentor.push(contactInfoMentor);
+  //     console.log("mentor" + mentor)
+  //     console.log()
+  //   }
+
+
+// find students in city
+  var studentsInCity = 0;
+  var studentsInCityArray = [];
+
+  // var cityNewPrompt = prompt("what city do you live in?")
+
+  function cityCheck() {
+    //don't i need to loop through them???
+    for (var cityNewPrompt in student) {
+      studentsInCity++;
+      console.log("studentsInCity" + studentsInCity)
+      studentsInCityArray.push(student.name);
+      console.log("studentsInCityArray" +studentsInCityArray)
+    // //need to create a list of the students...
+    //   studentsInCity.
+    }
+    return studentsInCity;
+
+    return studentsInCityArray;
+
+  }
+  cityCheck();
+
+
+
+
+
+// find students in city
+//   var studentsInCity = 0;
+
+//   function cityCheck(cityNewPrompt) {
+//     //don't i need to loop through them???
+//     if (students.city.indexOf(cityNewPrompt) != -1) {
+//       studentsInCity++;
+//     //need to create a list of the students...
+//       studentsInCity.
+//     }
+//     return studentsInCity;
+//   }
+
+// // return list of students in city
+//   var studentsInCityArray = [];
+//   function returnStudents() {
+//     for (var i = 0; i < studentsLog.length; i++) {
+//       if(studentLog[i].city === cityNewPrompt) {
+//         studentsInCityArray.push(studentsLog[i]);
+//           }
+//       }
+//       return studentsInCityArray;
+//   };
+
+
+
+
+// loop thro  ugh the studentarray of objects, grabbing the city.
+// if student city === ENTER CITY
+    //if mentor city === student city
